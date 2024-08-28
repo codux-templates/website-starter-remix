a1442010a5044bb18d20977fb8a2783a03e3c7c4
serhiiz <serhiiz@wix.com>
2024-08-28 21:54:58 +0200

    POC plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'node:path';
+import codux from './plugin';
 
 export default defineConfig({
     plugins: [
+        codux(),
         remix({
             ignoredRouteFiles: ['**/*.module.scss'],
         }),
