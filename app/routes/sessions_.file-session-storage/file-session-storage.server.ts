aab271cc1f79d974ad513ff01c06bb538256ce72
Noam Musba <noammu@wix.com>
2024-09-10 16:00:22 +0300

    fix: better name for the cookie

--- app/routes/sessions_.file-session-storage/file-session-storage.server.ts
+++ app/routes/sessions_.file-session-storage/file-session-storage.server.ts
@@ -3,5 +3,5 @@ import { createFileSessionStorage } from '@remix-run/node';
 // Create file-based session storage
 export const { getSession, commitSession } = createFileSessionStorage({
     dir: './sessions/file-session-storage',
-    cookie: { name: 'sessionId', path: '/sessions/file-session-storage' },
+    cookie: { name: 'file-storage-session', path: '/sessions/file-session-storage' },
 });
