dc277309abc95285ae14b1d191b2832b3f8ee9ac
ShireeWill <shiree@wix.com>
2024-09-17 16:33:48 +0300

    fixed ui kit typography

--- _codux/boards/ui-kits/ui-kit-typography.board.tsx
+++ _codux/boards/ui-kits/ui-kit-typography.board.tsx
@@ -15,21 +15,21 @@ export default createBoard({
 
             <h4 className={styles.sectionHeader}>HEADING</h4>
             <Variant name="Heading 1">
-                <h1 className={styles.heading1}>Heading 1</h1>
+                <h1 className={styles.heading1}>HEADING 1</h1>
             </Variant>
             <p className={classNames(styles.variantName, styles.headlinesSpacing)}>
                 --heading1:<span className={styles.fontDetails}> DM Sans (300) / 4rem / 1.4</span>
             </p>
 
             <Variant name="Heading 2">
-                <h6 className={classNames(styles.heading2)}>HEADING 6</h6>
+                <h6 className={classNames(styles.heading2)}>Heading 6</h6>
             </Variant>
             <p className={classNames(styles.variantName, styles.headlinesSpacing)}>
-                --heading2:<span className={styles.fontDetails}> DM Sans (400) / 1rem / 1.4</span>
+                --heading2:<span className={styles.fontDetails}> DM Sans (400) / 1rem / 1.3</span>
             </p>
 
             <hr className={styles.hrLight} />
-            <h4 className={styles.sectionHeader}>Paragraph</h4>
+            <h4 className={styles.sectionHeader}>PARAGRAPH</h4>
 
             <Variant name="Paragraph 1">
                 <p className={styles.paragraph1}>
@@ -38,7 +38,7 @@ export default createBoard({
             </Variant>
             <p className={classNames(styles.variantName, styles.paragraphSpacing)}>
                 --paragraph1:
-                <span className={styles.fontDetails}> DM Sans (300) / 1.3 / 1.3</span>
+                <span className={styles.fontDetails}> DM Sans (300) / 1.3rem / 1.3</span>
             </p>
 
             <Variant name="Paragraph 2">
@@ -47,7 +47,7 @@ export default createBoard({
                 </p>
             </Variant>
             <p className={classNames(styles.variantName, styles.paragraphSpacing)}>
-                --paragraph2:<span className={styles.fontDetails}> DM Sans (200) / 1rem / 1.4</span>
+                --paragraph2:<span className={styles.fontDetails}> DM Sans (200) / 1rem / 1.3</span>
             </p>
         </div>
     ),
